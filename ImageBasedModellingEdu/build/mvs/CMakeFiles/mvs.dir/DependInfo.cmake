dc75e57953086d756897d3b1b96ed308f0e2fe9e
daihangyu <1831805717@qq.com>
2021-01-23 13:10:47 +0800

    task1-add

---
+++ ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/dmrecon.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/dmrecon.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/global_view_selection.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/global_view_selection.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/image_pyramid.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/image_pyramid.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/local_view_selection.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/local_view_selection.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/mvs_tools.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/mvs_tools.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/patch_optimization.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/patch_optimization.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/patch_sampler.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/patch_sampler.cc.o"
+  "/home/daihangyu/git_code/ImageBasedModellingEdu/mvs/single_view.cc" "/home/daihangyu/git_code/ImageBasedModellingEdu/build/mvs/CMakeFiles/mvs.dir/single_view.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../mvs/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
